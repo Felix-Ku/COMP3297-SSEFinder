b183d40bf11c533fd9e224bbf99402332f54fb88
felix-ku <gta431525140@gmail.com>
2021-03-26 20:06:40 +0800

    update settings

--- CovidData/settings.py
+++ CovidData/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# from environs import Env
-# env = Env()
-# env.read_env()
+from environs import Env
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'el3i&=il$i^d+_!shgqg2o%dq%n#42o1u*!)tx#vsd^ny5d#g%'
-# SECRET_KEY = env('CHP_SECRET_KEY')
+SECRET_KEY = env('CHP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -79,14 +79,7 @@ WSGI_APPLICATION = 'CovidData.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'location',
-        'USER': 'felix',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+'default': env.dj_db_url('DATABASE_URL')
 }
 
 # Password validation
