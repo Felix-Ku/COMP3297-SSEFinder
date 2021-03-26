13d713720b12b8f0fb6279d6746d5e8f0900d226
felix-ku <gta431525140@gmail.com>
2021-03-26 20:57:50 +0800

    remove_testingpage.py

--- CovidDataPortal/urls.py
+++ CovidDataPortal/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('LocationAdd/', views.AddLocation, name='AddLocation'),
     path('LocationData_list/', views.LocationData_list_view, name='LocationData_list'),
-    path('Testing/', views.index_test, name='index_test'),
 ]
