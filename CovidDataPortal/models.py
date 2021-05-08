0fb84e0e689d0dabb3efd6630a16757c9a519c93
felix-ku <gta431525140@gmail.com>
2021-05-08 15:59:55 +0800

    Reverse admin page changes

--- CovidDataPortal/models.py
+++ CovidDataPortal/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 # Create your models here.
 class Location_data(models.Model):
@@ -11,17 +10,3 @@ class Location_data(models.Model):
     url = models.CharField(max_length=1000)
     def __str__(self): # Add string functions to models
         return self.name
-
-class Profile(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ID = models.CharField(max_length=30, blank=True)
-
-    def __str__(self):  # __unicode__ for Python 2
-        return self.user.username
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
