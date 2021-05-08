6fb495e8643be715c0bb40a17db82596aee961f2
felix-ku <gta431525140@gmail.com>
2021-05-08 15:49:13 +0800

    Add details for admin users

--- CovidDataPortal/models.py
+++ CovidDataPortal/models.py
@@ -12,28 +12,8 @@ class Location_data(models.Model):
     def __str__(self): # Add string functions to models
         return self.name
 
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='profile')
-
-    number = models.CharField('CHP staff number', max_length=6, blank=True)
-
-    class Meta:
-        verbose_name = 'User profile'
-
-    def __str__(self):
-        # return self.user.__str__()
-        return "{}".format(self.user.__str__())
-
-
 class Profile(models.Model):
-    Admin = 1
-    Staff = 2
-    ROLE_CHOICES = (
-        (Admin, 'Admin'),
-        (Staff, 'Staff'),
-    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     ID = models.CharField(max_length=30, blank=True)
 
