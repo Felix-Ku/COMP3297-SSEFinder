7e00dd1f9cc79e251292911cad1a26842a831c75
felix-ku <gta431525140@gmail.com>
2021-05-08 15:31:23 +0800

    Add details for admin users

--- CovidDataPortal/models.py
+++ CovidDataPortal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Location_data(models.Model):
@@ -8,3 +9,17 @@ class Location_data(models.Model):
     url = models.CharField(max_length=1000)
     def __str__(self): # Add string functions to models
         return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+
+    number = models.CharField('CHP staff number', max_length=6, blank=True)
+
+    class Meta:
+        verbose_name = 'User profile'
+
+    def __str__(self):
+        # return self.user.__str__()
+        return "{}".format(self.user.__str__())
