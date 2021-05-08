86d9b0bcf3f988b4ed4082f434cd2dc700750441
felix-ku <gta431525140@gmail.com>
2021-05-08 15:43:01 +0800

    Add details for admin users

--- CovidDataPortal/models.py
+++ CovidDataPortal/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Location_data(models.Model):
@@ -23,3 +25,23 @@ class UserProfile(models.Model):
     def __str__(self):
         # return self.user.__str__()
         return "{}".format(self.user.__str__())
+
+
+class Profile(models.Model):
+    Admin = 1
+    Staff = 2
+    ROLE_CHOICES = (
+        (Admin, 'Admin'),
+        (Staff, 'Staff'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ID = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
