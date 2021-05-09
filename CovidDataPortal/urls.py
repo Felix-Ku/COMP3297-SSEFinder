183b874680f7f4c43f69d8ce01897d49d4020b43
felix-ku <gta431525140@gmail.com>
2021-05-09 23:42:16 +0800

    Home login page - Modify

--- CovidDataPortal/urls.py
+++ CovidDataPortal/urls.py
@@ -6,12 +6,12 @@ from django.contrib.auth.views import LoginView
 app_name = 'CovidDataPortal'
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
+    path('CovidDataPortal/', views.index, name='CovidDataPortal'),
     #### Old urls
     # path('LocationAdd/', views.AddLocation, name='AddLocation'),
     # path('LocationData_list/', views.LocationData_list_view, name='LocationData_list'),
     #### New urls
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('', LoginView.as_view(template_name='login.html'), name="login"),
     path('Case_records/Create_record/', views.Create_record, name='Create_record'),
     path('Case_records/All_cases/', views.All_cases, name='All_cases'),
     path('Case_records/All_cases_success/', views.All_cases_success, name='All_cases_success'),
